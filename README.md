a5ca0961aeafe9d8ebfcf1a47269022d7d1c0731
Jorge Epuñan <jepunan@cl-macjespunan.local>
2017-01-03 17:04:34 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# hubot-emoji
-Hubot emoji find &amp; replace from a given string
+# Hubot Emoji
+
+**Hubot emoji find & replace from a given string**
+
+### Installation:
+
+````
+$ npm install hubot-emoji --save
+````
+
+Add in `external-scripts.json`:
+
+````
+[
+  ..
+  "hubot-emoji"
+]
+````
+
+### Use:
+
+````
+hubot> hubot emoji <string>
+````
+
+### License:
+[MIT](https://opensource.org/licenses/MIT)
+
---
+++ hubot-emoji.js
@@ -0,0 +1,36 @@
+// Description:
+//  Hubot emoji find & replace from a given string
+//
+// Dependencies:
+//  emoji-alphabet
+//
+// Commands:
+//  hubot emoji <query>
+
+var alphabet = require('emoji-alphabet').alphabet;
+
+function getRandom( arr ) {
+  return arr[ Math.floor( Math.random() * arr.length ) ];
+}
+
+module.exports = function(robot) {
+  return robot.respond(/emoji (\w+)$/i, function(msg) {
+
+    var i, len, letter, ref, str;
+    str = [];
+    ref = msg.match[1].toUpperCase().split("");
+
+    for (i = 0, len = ref.length; i < len; i++) {
+      letter = ref[i];
+
+      if (alphabet[letter]) {
+        str.push( getRandom(alphabet[letter]) );
+      } else {
+        str.push(letter);
+      }
+    }
+
+    msg.send(str.join(""));
+
+  });
+};
